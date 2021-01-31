572639769ecffd115ba14a5078b09619cfa25947
migu3l-hub <cachecuas1@gmail.com>
2021-01-31 15:42:57 -0600

    Primera version del script de montaje aun no probada

--- mountMaster.sh
+++ mountMaster.sh
@@ -11,7 +11,7 @@ SECONDS=15
 TRIES=30
 
 function validarMontaje() {
-  while [ $NODE != 3 ]; do
+  while [ $NODE -ge 3 ]; do
         NODE=$(docker node ls | grep -c Ready)
         echo "En espera de los demas nodos.."
         sleep 15
@@ -19,16 +19,20 @@ function validarMontaje() {
   until [  $COUNTER -eq "$TRIES" ]
     do
         let COUNTER=COUNTER+1
-        mount -a
-        if [ $? -eq 0 ]
-	        then
-	          MONTADO=1
-	        if [ $MONTADO -eq 1 ]
-	          then
-		          COUNTER=$TRIES
+        is_OK=$(docker exec -i "$(docker ps -qf name=ceph_mon)" ceph status | grep -c HEALTH_OK)
+        lines=$(docker exec -i "$(docker ps -qf name=ceph_mon)" ceph status | wc -l)
+        if [ $is_OK -eq 1 ] || [ $lines -lt 19 ]; then
+	          mount -a
+	          if [ $? -eq 0 ]; then
+	              MONTADO=1
 	          else
-		          MONTADO=0
-	        fi
+	              MONTADO=0
+	          fi
+            if [ $MONTADO -eq 1 ]; then
+                COUNTER=$TRIES
+              else
+                MONTADO=0
+            fi
 	      else
 	        sleep $SECONDS
         fi
@@ -37,12 +41,12 @@ function validarMontaje() {
 
 validarMontaje
 
-if [ $MONTADO -eq 1 ]
-    then
+if [ $MONTADO -eq 1 ]; then
           echo "montaje correcto"
 	        exit 0
-    else
+  else
           echo "paso por el else aun no montado"
           for i in $(docker service ls -qf name=ceph_mon) ; do docker service update $i ; done
+          COUNTER=0
           validarMontaje # -bailing, had to many tries
 fi
